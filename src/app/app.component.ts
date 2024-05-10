8386e196086dc0312d2185573cad5c32db3fa80c
dewinson1 <dewinson12@gmail.com>
2024-05-10 18:52:01 -0400

    V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NabvarComponent } from './nabvar/nabvar.component';
+import { ServiciosComponent } from './servicios/servicios.component'; 
+import { ContactosComponent } from './contactos/contactos.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NabvarComponent, ServiciosComponent, ContactosComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
